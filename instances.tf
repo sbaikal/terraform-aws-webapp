43e482ab5510caa19ee9bed38dc28b3f69340315
Baikal Sharshembiev <baikalchik07@gmail.com>
2022-02-16 22:42:03 -0600

    added keypair resource

--- instances.tf
+++ instances.tf
@@ -10,11 +10,17 @@ data "aws_ssm_parameter" "ami" {
 # RESOURCES
 ##################################################################################
 
+resource "aws_key_pair" "demo_auth" {
+  key_name   = "demokey"
+  public_key = file("~/.ssh/demokey.pub")
+}
+
 # INSTANCES #
 resource "aws_instance" "nginx" {
   count                  = var.instance_count
   ami                    = data.aws_ssm_parameter.ami.value
   instance_type          = var.instance_type
+  key_name               = aws_key_pair.demo_auth.id
   subnet_id              = module.vpc.public_subnets[count.index % length(module.vpc.public_subnets)]
   vpc_security_group_ids = [aws_security_group.nginx_sg.id]
 
